3749281d6d18c8f49a6bcf065f381ec88555725d
Dr. Moon 29 <pcacerloris@gmail.com>
2023-05-31 15:52:51 +0200

    2

--- navigation/screens/PersonalInfos.js
+++ navigation/screens/PersonalInfos.js
@@ -2,13 +2,15 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { horizontalScale, moderateScale, verticalScale } from '../utils/Metrics';
+import SelectDropdown from 'react-native-select-dropdown'
 
 export default function PersonalInfos({navigation, route}) {
 
     console.log(route.params.user);
 
 
-    
+    const countries = ["Egypt", "Canada", "Australia", "Ireland"]
+
     return(
         <>
         {
@@ -20,6 +22,23 @@ export default function PersonalInfos({navigation, route}) {
                     <Text style={{ fontSize: moderateScale(16), marginBottom: verticalScale(16) }}>Email : {route.params.user.email}</Text>
                     <Text style={{ fontSize: moderateScale(16), marginBottom: verticalScale(16) }}>Mot de passe : ********</Text>
                     <Text style={{ fontSize: moderateScale(16), marginBottom: verticalScale(16) }}>Site : {PersonalInfos.site}</Text>
+
+                    <SelectDropdown
+                        data={countries}
+                        onSelect={(selectedItem, index) => {
+                            console.log(selectedItem, index)
+                        }}
+                        buttonTextAfterSelection={(selectedItem, index) => {
+                            // text represented after item is selected
+                            // if data array is an array of objects then return selectedItem.property to render after item is selected
+                            return selectedItem
+                        }}
+                        rowTextForSelection={(item, index) => {
+                            // text represented for each item in dropdown
+                            // if data array is an array of objects then return item.property to represent item in dropdown
+                            return item
+                        }}
+                    />
                 </View>
             ) : (
                 <View style={{ flex : 1, alignItems: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'col'}}>
